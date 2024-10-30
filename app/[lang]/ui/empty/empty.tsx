c25d6f1b87691b5ee6c49712e6e446c97e2fd709
Muhiddin <88494561+Muhiddin0@users.noreply.github.com>
2024-10-30 14:59:48 +0500

    add not found langs

--- app/[lang]/ui/empty/empty.tsx
+++ app/[lang]/ui/empty/empty.tsx
@@ -1,17 +1,17 @@
+"use client";
+
 import { Newspaper } from "lucide-react";
+import { useTranslations } from "next-intl";
 import React from "react";
 
 function EmptyAnimation() {
+  const t = useTranslations("news.empty");
   return (
     <div className="w-full h-[80vh] flex items-center justify-center flex-col">
       <Newspaper size={100} />
-      <h1 className="lg:text-3xl text-xl font-semibold mt-4">
-        Yangiliklar majud emas
-      </h1>
+      <h1 className="lg:text-3xl text-xl font-semibold mt-4">{t("title")}</h1>
       <p className="text-center text-gray-500 text-sm lg:text-base mt-4 max-w-lg">
-        Biz sizni yangiliklar bilan tanishtiramiz. Ammo hozirgi vaqtda hech
-        qanday yangiliklar mavjud emas. Iltimos, kelajakda qaytib tashrif
-        buyuring.
+        {t("description")}
       </p>
     </div>
   );
