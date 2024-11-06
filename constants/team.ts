e17e4a67ed84ec2edf9f6e1e721d360af300e614
muhiddin0 <kabraliev2005@gmail.com>
2024-11-06 12:29:27 +0500

    change

--- constants/team.ts
+++ constants/team.ts
@@ -4,82 +4,82 @@ import AvatarNodirkhon from "@/assets/team/Nodirkhonov Anvar Nodirkhon ugli.jpg"
 import AvatarSadula from "@/assets/team/Sa'dulla Ne'matullaev.jpg";
 
 export const TeamMembersList = {
-  uz: [
-    {
-      fullname: "Nodirkhonov Anvar Nodirkhon ugli",
-      role: "Umumiy direktor",
-      avatar: AvatarNodirkhon,
-      phone: "+998 99 811 72 22",
-    },
-    {
-      fullname: "Anvar Irsaliev",
-      role: "Loyihalar boshqaruvchisi",
-      avatar: AvatarAnavar,
-      phone: "+998 99 811 72 22",
-    },
-    {
-      fullname: "Azizkhon Ishonkhujaev",
-      role: "Dasturiy ta'minot muhandisi",
-      avatar: AvatarAzizkhon,
-      phone: "+998 90 919 55 53",
-    },
-    {
-      fullname: "Sa'dulla Ne'matullaev",
-      role: "Dizayner",
-      avatar: AvatarSadula,
-      phone: "+998 90 942 49 49",
-    },
-  ],
-  en: [
-    {
-      fullname: "Nodirkhonov Anvar Nodirkhon ugli",
-      role: "General Director",
-      avatar: AvatarNodirkhon,
-      phone: "+998 99 811 72 22",
-    },
-    {
-      fullname: "Anvar Irsaliev",
-      role: "Project Manager",
-      avatar: AvatarAnavar,
-      phone: "+998 99 811 72 22",
-    },
-    {
-      fullname: "Azizkhon Ishonkhujaev",
-      role: "Software Engineer",
-      avatar: AvatarAzizkhon,
-      phone: "+998 90 919 55 53",
-    },
-    {
-      fullname: "Sa'dulla Ne'matullaev",
-      role: "Designer",
-      avatar: AvatarSadula,
-      phone: "+998 90 942 49 49",
-    },
-  ],
-  ru: [
-    {
-      fullname: "Нодирхонов Анвар Нодирхон угли",
-      role: "Генеральный директор",
-      avatar: AvatarNodirkhon,
-      phone: "+998 99 811 72 22",
-    },
-    {
-      fullname: "Анвар Ирсалиев",
-      role: "Менеджер проекта",
-      avatar: AvatarAnavar,
-      phone: "+998 99 811 72 22",
-    },
-    {
-      fullname: "Азизхон Ишонкужаев",
-      role: "Программист",
-      avatar: AvatarAzizkhon,
-      phone: "+998 90 919 55 53",
-    },
-    {
-      fullname: "Са'dулла Нематуллаев",
-      role: "Дизайнер",
-      avatar: AvatarSadula,
-      phone: "+998 90 942 49 49",
-    },
-  ],
+    uz: [
+        {
+            fullname: "Nodirkhonov Anvar Nodirkhon ugli",
+            role: "Umumiy direktor",
+            avatar: AvatarNodirkhon,
+            phone: "+998 99 811 72 22",
+        },
+        {
+            fullname: "Anvar Irsaliev",
+            role: "Loyihalar boshqaruvchisi",
+            avatar: AvatarAnavar,
+            phone: "+998 99 811 72 22",
+        },
+        {
+            fullname: "Azizkhon Ishonkhujaev",
+            role: "Dasturiy ta'minot muhandisi",
+            avatar: AvatarAzizkhon,
+            phone: "+998 90 919 55 53",
+        },
+        {
+            fullname: "Sa'dulla Ne'matullaev",
+            role: "Dizayner",
+            avatar: AvatarSadula,
+            phone: "+998 90 942 49 49",
+        },
+    ],
+    en: [
+        {
+            fullname: "Nodirkhonov Anvar Nodirkhon ugli",
+            role: "General Director",
+            avatar: AvatarNodirkhon,
+            phone: "+998 99 811 72 22",
+        },
+        {
+            fullname: "Anvar Irsaliev",
+            role: "Head of the import and export department",
+            avatar: AvatarAnavar,
+            phone: "+998 99 811 72 22",
+        },
+        {
+            fullname: "Azizkhon Ishonkhujaev",
+            role: "Manager for Europe and Gulf countries",
+            avatar: AvatarAzizkhon,
+            phone: "+998 90 919 55 53",
+        },
+        {
+            fullname: "Sa'dulla Ne'matullaev",
+            role: "Manager for Turkey and other countries",
+            avatar: AvatarSadula,
+            phone: "+998 90 942 49 49",
+        },
+    ],
+    ru: [
+        {
+            fullname: "Нодирхонов Анвар Нодирхон угли",
+            role: "Генеральный директор",
+            avatar: AvatarNodirkhon,
+            phone: "+998 99 811 72 22",
+        },
+        {
+            fullname: "Анвар Ирсалиев",
+            role: "Менеджер проекта",
+            avatar: AvatarAnavar,
+            phone: "+998 99 811 72 22",
+        },
+        {
+            fullname: "Азизхон Ишонкужаев",
+            role: "Программист",
+            avatar: AvatarAzizkhon,
+            phone: "+998 90 919 55 53",
+        },
+        {
+            fullname: "Са'dулла Нематуллаев",
+            role: "Дизайнер",
+            avatar: AvatarSadula,
+            phone: "+998 90 942 49 49",
+        },
+    ],
 };
