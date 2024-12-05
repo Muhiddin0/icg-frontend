5129106d19e9dabb09490f87bfe9c99b1f75c9f5
muhiddin0 <kabraliev2005@gmail.com>
2024-12-05 16:24:34 +0500

    add yandex metrik

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,27 +4,28 @@ import { ToastContainer } from "react-toastify";
 
 import "./globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import YandexMetrika from "./yandex-analitc";
 
 function ExternalLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <html>
-            <body>
-                {children}
-                <ToastContainer
-                    position="bottom-right"
-                    autoClose={5000}
-                    hideProgressBar
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="light"
-                />
-            </body>
-        </html>
-    );
+  return (
+    <html>
+      <body>
+        <YandexMetrika>{children}</YandexMetrika>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </body>
+    </html>
+  );
 }
 
 export default ExternalLayout;
