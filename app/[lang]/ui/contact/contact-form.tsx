1612f9189b33ee8fe641e15be62a0ec9a1dfa0d4
Muhiddin <88494561+Muhiddin0@users.noreply.github.com>
2024-12-26 21:57:03 +0500

    add sugg4sion change

--- app/[lang]/ui/contact/contact-form.tsx
+++ app/[lang]/ui/contact/contact-form.tsx
@@ -23,7 +23,7 @@ function ContactForm({}: Props) {
       email: email,
       message: message,
       name: name,
-      subject: "Bog'lanish",
+      subject: "Shikoyat",
     })
       .then(() => {
         toast(notifications("success"));
@@ -38,10 +38,10 @@ function ContactForm({}: Props) {
   return (
     <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
       <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-        {t("contactForm.heading")}
+        {t("sectionTitle.title")}
       </h2>
       <p className="leading-relaxed mb-5 text-gray-600">
-        {t("contactForm.description")}
+        {t("sectionTitle.description")}
       </p>
       <div className="relative mb-4">
         <label htmlFor="email" className="leading-7 text-sm text-gray-600">
--- app/[lang]/ui/contact/contact-section.tsx
+++ app/[lang]/ui/contact/contact-section.tsx
@@ -4,38 +4,38 @@ import { useTranslations } from "next-intl";
 import ContactForm from "./contact-form";
 
 function ContactSection() {
-    const t = useTranslations("home.contact");
-    return (
-        <section>
-            <div className="container px-5 pt-24 mx-auto">
-                <SectionTitle
-                    title={t("sectionTitle.title")}
-                    desk={t("sectionTitle.description")}
-                />
-            </div>
+  const t = useTranslations("home.contact");
+  return (
+    <section>
+      <div className="container px-5 pt-24 mx-auto">
+        <SectionTitle
+          title={t("sectionTitle.title")}
+          desk={t("sectionTitle.description")}
+        />
+      </div>
 
-            <div className="text-gray-600 body-font relative mt-12">
-                <div className="absolute inset-0 bg-gray-300">
-                    <iframe
-                        width="100%"
-                        height="100%"
-                        frameBorder={0}
-                        marginHeight={0}
-                        marginWidth={0}
-                        title="map"
-                        scrolling="no"
-                        src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=41.2988889,69.3272222&ie=UTF8&t=&z=14&iwloc=B&output=embed"
-                        style={{
-                            filter: "grayscale(1) contrast(1.2) opacity(0.4)",
-                        }}
-                    />
-                </div>
-                <div className="container px-5 py-24 mx-auto flex">
-                    <ContactForm />
-                </div>
-            </div>
-        </section>
-    );
+      <div className="text-gray-600 body-font relative mt-12">
+        <div className="absolute inset-0 bg-gray-300">
+          <iframe
+            width="100%"
+            height="100%"
+            frameBorder={0}
+            marginHeight={0}
+            marginWidth={0}
+            title="map"
+            scrolling="no"
+            src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=41.2988889,69.3272222&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+            style={{
+              filter: "grayscale(1) contrast(1.2) opacity(0.4)",
+            }}
+          />
+        </div>
+        <div className="container px-5 py-24 mx-auto flex">
+          <ContactForm />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ContactSection;
